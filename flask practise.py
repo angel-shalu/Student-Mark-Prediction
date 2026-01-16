7dc997bce5838fb413742c1bb1c4267a46b4e423
angel-shalu <shalinikumari8789@gamil.com>
2026-01-16 10:41:59 +0530

    completed

---
+++ flask practise.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Welcome to flask Today we try First App'
+
+#app.run(host='0.0.0.0', port=81)
+
+app.run(host ='127.0.0.1', port=5000)
+
+
+
